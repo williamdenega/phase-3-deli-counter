40209e972ff4c7713acc3de1e2bb79f0fad9dad1
William Denega <williamdenega@gmail.com>
2023-10-27 17:15:03 -0400

    commit tree 979e064c6acbcfee0462361299ffb8736233786a

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
+require 'pry'
 # Write your code here.
+
+def line array
+    case array.length
+    when 0
+        puts "The line is currently empty."
+    else 
+        formatted = array.each_with_index.map {|name, index| "#{index + 1}. #{name}"}
+        puts "The line is currently: " + formatted.join(' ')
+    end
+end
+
+def take_a_number( array, person)
+    if array.include?(person)
+        position = array.index(person) + 1
+        puts "Welcome, #{person}. You are number #{position} in line."
+    else
+        array << person
+        position = array.index(person) + 1
+        puts "Welcome, #{person}. You are number #{position} in line."
+    end
+end
+
+
+def now_serving (array)
+    case array.length
+    when 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
+
+
+binding.pry
